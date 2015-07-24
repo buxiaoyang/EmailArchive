ddea20c0f0d9faee2c4189303c4dc2e13e03b7b8
Bu, Xiao-Yang <buxiaoyang@gmail.com>
2015-07-24 11:17:08 +0800

    init commit.

---
+++ config-site.php
@@ -0,0 +1,27 @@
+<?php
+
+$config['SITE_NAME'] = '192.168.20.131';
+$config['SITE_URL'] = 'http://192.168.20.131/';
+$config['DIR_BASE'] = '/var/www/piler.yourdomain.com/';
+
+$config['TIMEZONE'] = 'Europe/Budapest';
+
+$config['ENABLE_SYSLOG'] = 1;
+
+$config['SMTP_DOMAIN'] = 'piler.yourdomain.com';
+$config['SMTP_FROMADDR'] = 'no-reply@piler.yourdomain.com';
+$config['ADMIN_EMAIL'] = 'admin@piler.yourdomain.com';
+
+$config['DB_DRIVER'] = 'mysql';
+$config['DB_PREFIX'] = '';
+$config['DB_HOSTNAME'] = 'localhost';
+$config['DB_USERNAME'] = 'piler';
+$config['DB_PASSWORD'] = 'piler123';
+$config['DB_DATABASE'] = 'piler';
+
+$config['PILER_HOST'] = '192.168.100.100';
+
+$config['SMARTHOST'] = '192.168.100.99';
+$config['SMARTHOST_PORT'] = 25;
+
+?>
